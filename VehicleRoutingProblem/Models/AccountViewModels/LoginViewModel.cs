3b74112bc8e57ca245c35dabd605f4c82f27c3ee
Amir Bokaeyan <am.bo740@gmail.com>
2017-09-05 14:35:59 +0430

    پایان بخش اول در یک شاخه دیگر روی صفحه ورودی کار می کنم

--- VehicleRoutingProblem/Models/AccountViewModels/LoginViewModel.cs
+++ VehicleRoutingProblem/Models/AccountViewModels/LoginViewModel.cs
@@ -10,13 +10,15 @@ namespace VehicleRoutingProblem.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
+        [Display(Name ="ایمیل")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "من را به خاطر بسپار ؟")]
         public bool RememberMe { get; set; }
     }
 }
