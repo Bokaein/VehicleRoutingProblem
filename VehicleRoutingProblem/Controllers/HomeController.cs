2e968fbe5fb2ad89b323844284e685912667ae51
Amir Bokaeyan <am.bo740@gmail.com>
2017-09-21 08:30:02 +0330

    first comit

--- VehicleRoutingProblem/Controllers/HomeController.cs
+++ VehicleRoutingProblem/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace VehicleRoutingProblem.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            
             return View();
         }
 
